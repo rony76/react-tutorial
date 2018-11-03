caba8ee2ab4fa6025c50c22938180b11668a7c7b
Rony Cesana <rony.cesana@gmail.com>
2018-11-03 12:57:57 +0100

    Fixed configuration: react preset was not included!

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ var config = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-env', '@babel/react']
                     }
                 }
             }
