27d2811bfcfe439b960e666206551b0612d64c76
Rony Cesana <rony.cesana@gmail.com>
2018-11-03 19:24:23 +0100

    Turned class component to pure component

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-export class Header extends React.Component {
-    render() {
-        return (
-            <nav className="navbar navbar-default">
-                <div className="navbar-header">
-                    <ul className="nav navbar-nav">
-                        <li className="active"><a href="#">Home</a></li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
-}
+export const Header = (props) => {
+    return (
+        <nav className="navbar navbar-default">
+            <div className="navbar-header">
+                <ul className="nav navbar-nav">
+                    <li className="active"><a href="#">Home</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
